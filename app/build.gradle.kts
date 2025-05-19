e1ca6940db325fc45ddaebf592d9c4c8fcbf5fe6
majemase <majemase12@gmail.com>
2025-05-19 17:59:21 +0200

    Implementacion de firebase y google analytics en el proyecto he inicio de mdelo de login y registro

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
+
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -32,6 +34,9 @@ android {
 }
 
 dependencies {
+
+    implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
+    implementation("com.google.firebase:firebase-analytics")
 
     implementation(libs.appcompat)
     implementation(libs.material)
---
+++ app/src/main/java/com/example/gamehub/model/registro.java
@@ -0,0 +1,4 @@
+package com.example.gamehub.model;
+
+public class registro {
+}
